8e348a0b2eeb3cf03f51c4ea76549754fa63e6a7
Moshe Dayan <xynpyro@gmail.com>
2023-12-13 16:29:27 +0700

    First Commit
    All Done, Except some Interactive Features and Login Forms Validation

---
+++ script.js
@@ -0,0 +1,6 @@
+var jQueryScript = document.createElement("script");
+jQueryScript.setAttribute(
+  "src",
+  "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+);
+document.head.appendChild(jQueryScript);
